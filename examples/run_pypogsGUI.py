27c2136af3d3192a4530cdaa15002aa710fc4873
rkinnett <rkinnett@gmail.com>
2021-12-20 15:46:18 -0800

    updated my own startup script

--- examples/run_pypogsGUI.py
+++ examples/run_pypogsGUI.py
@@ -13,21 +13,22 @@ import pypogs
 sys = pypogs.System()
 
 try:
-  sys.add_mount(model="ascom", identity="ASCOM.DeviceHub.Telescope")
+  #sys.add_mount(model="ascom", identity="Simulator")
+  sys.add_mount(model="ascom", identity="DeviceHub")
   sys.mount.axis_directions = (1,-1)
 except:  pass
 
 
 try:
-  #sys.add_coarse_camera(model="ascom", identity="ASCOM.Simulator.Camera")
-  sys.add_coarse_camera(model="ascom", identity="ASCOM.ASICamera2_2.Camera")
+  #sys.add_coarse_camera(model="ascom", identity="Simulator")
+  sys.add_coarse_camera(model="ascom", identity="ASICamera2_2")
   sys.coarse_camera.exposure_time = 200
   try:  sys.coarse_camera.gain = 400
   except: pass
   sys.coarse_camera.plate_scale = 4.5
 except: pass
   
-  
+
 sys.alignment.set_location_lat_lon(lat=34.2, lon=-118.2, height=600)
 sys.alignment.set_alignment_enu()
 try:
